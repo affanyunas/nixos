a88fa1dbe463c6de6819022334b47a93e86b1270
affanyunas <74919708+affanyunas@users.noreply.github.com>
2023-11-26 15:05:56 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+	  ./programs.nix
     ];
 
   # Bootloader.
@@ -21,6 +22,18 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable Some Hardware 
+  hardware.bluetooth.enable = true;
+  # Fonts
+  fonts.packages = with pkgs; [
+    noto-fonts-emoji
+    dejavu_fonts
+    liberation_ttf
+    source-code-pro
+    inter
+    (nerdfonts.override { fonts = [ "JetBrainsMono" "Iosevka" ]; })
+  ];
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -46,14 +59,33 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Enable BSPWM
+  services.xserver.windowManager.bspwm.enable = true;
+  services.xserver.displayManager = { 
+    defaultSession = "none+bspwm"; 
+    setupCommands = ''
+    my_laptop_external_monitor=$(${pkgs.xorg.xrandr}/bin/xrandr --query | grep 'DP-3 connected')
+    if [[ $my_laptop_external_monitor = *connected* ]]; then
+      ${pkgs.xorg.xrandr}/bin/xrandr --output DP-3 --primary --mode 3440x1440 --rate 100 --output eDP-1 --off
+    else
+      ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --mode 1920x1200 --rate 60
+    fi
+    ''; 
+  };
   # Enable the Deepin Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.deepin.enable = true;
+
+  services.xserver.desktopManager.deepin.enable = false;
+  services.xserver.displayManager.lightdm = {
+    enable = true;
+    greeters.enso.enable = true;
+  };
+  
 
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
+	autorun = false;
   };
 
   # Enable CUPS to print documents.
@@ -84,12 +116,9 @@
     isNormalUser = true;
     description = "fan";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-	firefox
-	git
-	vscode
-    ];
+	
   };
+  users.extraUsers.fan = { shell = pkgs.zsh;};
 
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
@@ -101,9 +130,81 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    # App Dependencies
+    cargo
+
+    # Xorg
+    xorg.xbacklight
+
+    # Global Apps
+    wget
+    neovim
+   
+  
+    git
+  
+    bat
+ 
+    htop
+    
+	brave
+
+    # Desktop Environment
+    polybar
+    alacritty
+    sxhkd
+    picom
+    pavucontrol
+    blueberry
+    xclip
+    rofi
+    dunst
+    flameshot
+    nitrogen
+    networkmanagerapplet
+    i3lock-color
+    arandr
+    xfce.xfce4-power-manager
+
+    # Theming
+    qogir-theme
+    papirus-icon-theme
+    rose-pine-gtk-theme
+
+    # Files
+    unzip
+    ranger
+    ueberzug
+    udiskie
+    xfce.thunar
+    xfce.thunar-volman
+    xfce.thunar-archive-plugin
   ];
+
+  services.xserver.excludePackages = [ pkgs.xterm ];
+
+  virtualisation.docker.enable = true;
+  environment.variables.EDITOR = "nvim";
+  nixpkgs.overlays = [
+    (self: super: {
+      neovim = super.neovim.override {
+        viAlias = true;
+        vimAlias = true;
+      };
+    })
+  ];
+
+  nixpkgs.config.packagesOverrides = pkgs: {
+    polybar = pkgs.polybar.override {
+      jackSupport = true;
+      alsaSupport = true;
+      githubSupport = true;
+      mpdSupport = true;
+      pulseSupport = true;
+      iwSupport = true;
+      nlSupport = true;
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -112,11 +213,11 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
+  system.autoUpgrade.enable = true;
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -124,12 +225,19 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  #Optimising the store
+  #nix.optimise.automatic = true;
+  # Garbage Collection
+  #nix.gc = {
+  #	automatic = true;
+  #	dates = "weekly";
+  #	options = "--delete-older-than 30d";
+  #	};
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+  system.stateVersion = "23.05"; # Did you read the comment? 
 }

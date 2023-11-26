143d6d6d709fa6594981a2a6a08a055475647b92
affanyunas <74919708+affanyunas@users.noreply.github.com>
2023-11-26 15:07:21 +0700

    Update programs.nix

--- programs.nix
+++ programs.nix
@@ -4,10 +4,10 @@
 {
 	#ZSH
 	programs.zsh = {
-    enable = true;
-    ohMyZsh.enable = true;
-    ohMyZsh.plugins = [ "git" "npm" "vi-mode" ];
-    syntaxHighlighting.enable = true;
-    autosuggestions.enable = true;
+	enable = true;
+	ohMyZsh.enable = true;
+    	ohMyZsh.plugins = [ "git" "npm" "vi-mode" ];
+	syntaxHighlighting.enable = true;
+	autosuggestions.enable = true;
   };
 }
